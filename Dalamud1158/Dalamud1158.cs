a17acf7aeb4746f552ee6722c52a885aec12d465
liam <lmcintyre995@gmail.com>
2023-03-14 13:12:36 -0400

    Initial commit

---
+++ Dalamud1158/Dalamud1158.cs
@@ -0,0 +1,27 @@
+﻿using Dalamud.Plugin;
+using Dalamud.IoC;
+using Dalamud.Logging;
+using FFXIVClientStructs.FFXIV.Client.Graphics.Kernel;
+
+namespace Dalamud1158
+{
+    public class Dalamud1158 : IDalamudPlugin
+    {
+        public string Name => "Dalamud1158";
+
+        private const string CommandName = "/dalamud1158";
+
+        public unsafe Dalamud1158([RequiredVersion("1.0")] DalamudPluginInterface pluginInterface)
+        {
+            DalamudContainer.Initialize(pluginInterface);
+
+            var test = new SharpDX.Direct3D11.Device((nint)Device.Instance()->D3D11Forwarder);
+            var contextTest = test.ImmediateContext;
+            PluginLog.Debug($"Device: {test.NativePointer:X} Context: {contextTest.NativePointer:X}");
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
